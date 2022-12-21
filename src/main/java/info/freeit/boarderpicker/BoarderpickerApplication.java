1f71361babcc6353f89daf5cafcdd07ed418dd82
Irina <qatirina7@gmail.com>
2022-12-21 22:46:00 +0400

    Initial commit

---
+++ src/main/java/info/freeit/boarderpicker/BoarderpickerApplication.java
@@ -0,0 +1,13 @@
+package info.freeit.boarderpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoarderpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoarderpickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/freeit/boarderpicker/BoarderpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package info.freeit.boarderpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoarderpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
