962f28ff91cf4af511e37157c5fdf48f5ad6bd33
ilya-frolov <ilyafrolov0711@gmail.com>
2023-01-06 13:55:50 +0200

    Delete CategoryService and ProducerService since addressing from GameService now straight to their repositories.

--- src/main/java/info/freeit/boarderpicker/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package info.freeit.boarderpicker.service;
-
-import info.freeit.boarderpicker.entity.Category;
-
-public interface CategoryService {
-
-    void saveCategory (Category category);
-    Category getCategoryByName (String name);
-}
