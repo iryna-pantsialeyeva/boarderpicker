1d93b9ee6f57cc3d82325c16a75a3c80706761ae
ilya-frolov <ilyafrolov0711@gmail.com>
2023-01-06 23:23:21 +0200

    1. Return UserRepository to CrudRepository. 2. Return SpringBoot to version 3.0 -> Change pom.xml 3. Change all imports to Jacarta according to SpringBoot 3 requirements.

--- src/main/java/info/freeit/boarderpicker/entity/Game.java
+++ src/main/java/info/freeit/boarderpicker/entity/Game.java
@@ -1,23 +1,23 @@
 package info.freeit.boarderpicker.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -55,6 +55,6 @@ public class Game {
 
 //    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "producers_id", referencedColumnName = "id", nullable = true)
+    @JoinColumn(name = "producers_id", nullable = true)
     private Producer producer;
 }
--- src/main/java/info/freeit/boarderpicker/entity/Sale.java
+++ src/main/java/info/freeit/boarderpicker/entity/Sale.java
@@ -1,19 +1,18 @@
 package info.freeit.boarderpicker.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "sales")
